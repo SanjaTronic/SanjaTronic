3c350c6dbd288f76f46aa04aa5ed99887084339f
sanjaTronic <mazesanjatronic@gmail.com>
2026-05-12 16:56:07 -0300

    Alterações realizadas: . Calibração do encoder; . Implementação da lógica de andar de 30 em 30 centímetros.

--- Maze_2026/testes_sensores_individuais/testeEncoder/testeEncoder.ino
+++ Maze_2026/testes_sensores_individuais/testeEncoder/testeEncoder.ino
@@ -1,3 +1,15 @@
+/*
+============================= TESTE ENCODER ====================================
+Autor: Murilo Siqueira Santos
+Data da criação: 09/04/2026
+Data da última modificação: 12/05/2026
+
+Campo de observações:
+  . uma volta da roda equivale a 40 recortes
+  . um ladrilho percorrido equivale a 46 recortes
+================================================================================
+*/
+
 int sensorPin = 8; 
 int leituraSensor; 
 int leituraAnterior = 0;  // guarda o estado anterior
@@ -7,6 +19,7 @@ int voltaRoda = 0;
 void setup() 
 {
   Serial.begin(9600); 
+  Serial.println("Santos futebol clube");
   pinMode(sensorPin, INPUT);
 }
 
@@ -20,10 +33,11 @@ void loop()
     contagem++;
 
 
-    if(contagem % 40 == 0)
+    if(contagem % 46 == 0)
     {
       voltaRoda++;
-      Serial.print("Numero de voltas da roda: ");
+      //Serial.print("Numero de voltas da roda: ");
+      Serial.print("números de ladrilhos percorridos: ");
       Serial.println(voltaRoda);
     }
   }
